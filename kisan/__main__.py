625419a7b87afbda331f28f4022531a26e6dec21
aditkum <aadityakumarsahroy@gmail.com>
2025-06-30 23:02:02 +0700

    Update __main__.py

--- kisan/__main__.py
+++ kisan/__main__.py
@@ -3,7 +3,6 @@ import logging
 from os import getenv
 from pyrogram import Client, filters, idle
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.errors import ChatAdminRequired
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -12,51 +11,47 @@ logging.basicConfig(
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 # config vars
-API_ID = os.getenv("20981861")
-API_HASH = os.getenv("7f63fa7339bee2f21988871df768b555")
-BOT_TOKEN = os.getenv("7815597466:AAEZQnI1vtQTMS2x8bpOqsUp1eCD16JrGN0")
-OWNER = os.getenv("6967953022")
+API_ID = int(os.getenv("API_ID"))  # Pastikan variabel lingkungan disetel
+API_HASH = os.getenv("API_HASH")    # Pastikan variabel lingkungan disetel
+BOT_TOKEN = os.getenv("BOT_TOKEN")   # Pastikan variabel lingkungan disetel
+OWNER = os.getenv("OWNER")           # Pastikan variabel lingkungan disetel
 
 # pyrogram client
 app = Client(
-            "banall",
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
+    "banall",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
 )
 
 @app.on_message(
-filters.command("start")
-& filters.private            
+    filters.command("start") & filters.private
 )
 async def start_command(client, message: Message):
-  await message.reply_photo(
-                            photo = f"https://telegra.ph/file/fff2ee6f504bc061cb7d3.jpg",
-                            caption = f"ʜᴇʏ, ᴛʜɪs ɪs ᴀ sɪᴍᴘʟᴇ ʙᴀɴ ᴀʟʟ ʙᴏᴛ ᴡʜɪᴄʜ ɪs ʙᴀsᴇᴅ ᴏɴ ᴘʏʀᴏɢʀᴀᴍ ʟɪʙᴇʀᴀʀʏ ᴛᴏ ʙᴀɴ ᴏʀ ᴅᴇsᴛʀᴏʏ ᴀʟʟ ᴛʜᴇ ᴍᴇᴍʙᴇʀs ғʀᴏᴍ ᴀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ɪɴ ᴀ ғᴇᴡ  sᴇᴄᴏɴᴅs!\n\nᴛᴏ ᴄʜᴇᴄᴋ ᴍʏ ᴀʙɪʟɪᴛʏ ɢɪʙ me ғᴜʟʟ ᴘᴏᴡᴇʀs\n\nᴛʏᴘᴇ /ʙᴀɴᴀʟʟ ᴛᴏ ꜱᴇᴇ ᴍᴀɢɪᴄ ɪɴ ɢʀᴏᴜᴘ.",
-  reply_markup=InlineKeyboardMarkup(
+    await message.reply_photo(
+        photo="https://telegra.ph/file/fff2ee6f504bc061cb7d3.jpg",
+        caption="ʜᴇʏ, ᴛʜɪs ɪs ᴀ sɪᴍᴘʟᴇ ʙᴀɴ ᴀʟʟ ʙᴏᴛ ᴡʜɪᴄʜ ɪs ʙᴀsᴇᴅ ᴏɴ ᴘʏʀᴏɢʀᴀᴍ ʟɪʙᴇʀᴀʀʏ ᴛᴏ ʙᴀɴ ᴏʀ ᴅᴇsᴛʀᴏʏ ᴀʟʟ ᴛʜᴇ ᴍᴇᴍʙᴇʀs ғʀᴏᴍ ᴀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ɪɴ ᴀ ғᴇᴡ sᴇᴄᴏɴᴅs!\n\nᴛᴏ ᴄʜᴇᴄᴋ ᴍʏ ᴀʙɪʟɪᴛʏ ɢɪʙ me ғᴜʟʟ ᴘᴏᴡᴇʀs\n\nᴛʏᴘᴇ /ʙᴀɴᴀʟʟ ᴛᴏ ꜱᴇᴇ ᴍᴀɢɪᴄ ɪɴ ɢʀᴏᴜᴘ.",
+        reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(
-                        "ᴏᴡɴᴇʀ", url=f"https://t.me/{OWNER}")
-                ]       
-           ]
-      )
-)
+                    InlineKeyboardButton("ᴏᴡɴᴇʀ", url=f"https://t.me/{OWNER}")
+                ]
+            ]
+        )
+    )
 
 @app.on_message(
-filters.command("banall") 
-& filters.group
+    filters.command("banall") & filters.group
 )
 async def banall_command(client, message: Message):
-    print("getting memebers from {}".format(message.chat.id))
+    print("getting members from {}".format(message.chat.id))
     async for i in app.get_chat_members(message.chat.id):
         try:
-            await app.ban_chat_member(chat_id = message.chat.id, user_id = i.user.id)
+            await app.ban_chat_member(chat_id=message.chat.id, user_id=i.user.id)
             print("kicked {} from {}".format(i.user.id, message.chat.id))
         except Exception as e:
-            print("failed to kicked {} from {}".format(i.user.id, e))           
+            print("failed to kick {}: {}".format(i.user.id, e))
     print("process completed")
-    
 
 # start bot client
 app.start()
